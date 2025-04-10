cbe4fe5cdfb74b957c4a33de73665aba609ff4f7
Dennis <dennis.gonzalez01@estudiant.upf.edu>
2025-04-10 13:23:19 +0200

    Prueba Dennis

--- src/main.c
+++ src/main.c
@@ -15,8 +15,8 @@ int main() {
   // how to import and call a function
   printf("Factorial of 4 is %d\n", fact(4));
 
-  // uncomment and run "make v" to see how valgrind detects memory leaks
-  // createaleak();
+
+  createaleak();
 
   return 0;
 }
