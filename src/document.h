837fd14373aba662a3a454eb719a29ef7e2f6fd5
Dennis <dennis.gonzalez01@estudiant.upf.edu>
2025-05-08 13:35:19 +0200

    Lab 3 puntos 1 y 2

--- src/document.h
+++ src/document.h
@@ -1,26 +1,34 @@
 #ifndef DOCUMENT_H
 #define DOCUMENT_H
 
-typedef struct{
-    int id;
+typedef struct {
+  int id;
 } Links;
 
-typedef struct{
-    int id;
-    Links list_links;
-    char title[50];
-    char body[];
+typedef struct {
+  int id;
+  Links list_links;
+  char title[50];
+  char body[];
 } Document;
 
-typedef struct{
-    char word[50];
-} QueryItem;
-
-typedef struct{
-    QueryItem linked_list;
+typedef struct Query {
+  char* query_string;
 } Query;
 
-Links* LinksInit();
+typedef struct Node {
+  Query* query;
+  struct Node* next;
+} Node;
+Node* insert_query(Node* head, char* query_string) {
+  Node* new_node = (Node*)malloc(sizeof(Node));
+  new_node->query = (Query*)malloc(sizeof(Query));
+  new_node->query->query_string = query_string;
+  new_node->next = head;
+  return new_node;
+};
+
+Links *LinksInit();
 Document *document_desserialize(char *path);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,10 +1,22 @@
+#include "document.c"
+#include "document.h"
 #include "sample_lib.h"
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include "document.c"
-#include "document.h"
+
+typedef struct Hashmap{
+  Node* hashmap;
+  int size;
+  int count;
+} *Hashmap;
+
+typedef struct{
+  char *word;
+  int *document_ids;
+  int *document_cuenta;
+} ReverseindexHashmap;
 
 void createaleak() {
   char *foo = malloc(20 * sizeof(char));
@@ -16,7 +28,7 @@ int main() {
 
   // how to import and call a function
   printf("Factorial of 4 is %d\n", fact(4));
-  //createaleak();
+  // createaleak();
 
   return 0;
 }
