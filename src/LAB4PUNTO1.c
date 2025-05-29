5c0c0660031e306575b8c7f5f5c31ac67a8cd869
TKone201 <leon.bescos01@estudiant.upf.edu>
2025-05-29 16:25:03 +0200

    Update LAB4PUNTO1.c

--- src/LAB4PUNTO1.c
+++ src/LAB4PUNTO1.c
@@ -1,27 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "LAB 1 PUNTO 3.c"
 
-typedef struct{
-    int num_nodes;
-    int **matrix;
-} Directedgraph;
-
-Directedgraph *create_graph(int num_nodes){
-    Directedgraph *new_graph = (Directedgraph*) malloc(sizeof(Directedgraph));
-    new_graph->num_nodes = num_nodes;
-    new_graph->matrix = (int**) malloc(num_nodes*sizeof(int*));
-    for (int i = 0; i < num_nodes; ++i){
-        new_graph->matrix[i] = (int*) malloc(num_nodes*sizeof(int));
-        for (int j = 0; j < num_nodes; ++j){
-            new_graph->matrix[i][j] = 0;
-        }
-    }
-    return new_graph;
-}
-
-void add_edge(Directedgraph* graph, Document* origin, Document* destiny){
-    graph->matrix[origin->id][destiny->id] = 1;
-}
 
 
