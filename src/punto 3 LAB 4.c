b5dc46f2a5c78b9119c80ffbd077afd33e98bd7d
paugaarridoo <pau.garrido01@estudiant.upf.edu>
2025-05-29 11:24:00 +0200

    Update punto 3 LAB 4.c

--- src/punto 3 LAB 4.c
+++ src/punto 3 LAB 4.c
@@ -1,12 +1,13 @@
 typedef struct { //QUICKSORT
     char* doc_id;       // NOMBRE PARA ORDENAR DEL ARCHIVO
-    int relevance;      // PUNTUAJE QUE SE ATRIBUIRA AL DOC
+    int relevancia;      // función PUNTUAJE QUE SE ATRIBUIRA AL DOC
 } Document;
-int compare_relevance(const void* a, const void* b) {
+int compara_relevancia(const void* a, const void* b) {
     Document* docA = (Document*)a; //DOCUMENTO A
     Document* docB = (Document*)b; //DOCUMENTO B  LUEGO LOS COMPARARÁ PARA HACER EL QUICKSORT
     return docB->relevance - docA->relevance;  //ATRIBUIMOS EL ORDEN QUE USAREMOS, ESTE SERÁ DESCENDENTE
 }
-void sort_documents_by_relevance(Document* docs, int count) {
-    qsort(docs, count, sizeof(Document), compare_relevance);
+void sort_documents_por_revelancia(Document* docs, int count) {
+    qsort(docs, count, sizeof(Document), compara_relevancia); //LLAMAMOS A LA FUNCIÓN PARA QUE SE EJECUTE
 }
+ 
