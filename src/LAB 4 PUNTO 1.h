2a0d3ae07584b1cb69b6bfb97da02c773305d58a
TKone201 <leon.bescos01@estudiant.upf.edu>
2025-05-29 17:03:40 +0200

    Update LAB 4 PUNTO 1.h

--- src/LAB 4 PUNTO 1.h
+++ src/LAB 4 PUNTO 1.h
@@ -1,19 +1,28 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+//nodo que guarda a donde apunta y el siguiente en la lista
 typedef struct GraphNode {
     int target_id;
     struct GraphNode* next;
 } GraphNode;
 
+//grafo con cantidad de nodos y lista de adyacencia
 typedef struct {
     int num_nodes;
     GraphNode** adj_list;
 } DirectedGraph;
 
+//crea el grafo
 DirectedGraph* create_graph(int num_nodes);
+
+//agrega una flecha de un nodo a otro
 void add_edge(DirectedGraph* graph, int from_id, int to_id);
+
+//cuenta cuantas flechas llegan al nodo
 int graph_get_indegree(DirectedGraph* graph, int doc_id);
+
+//libera todo
 void free_graph(DirectedGraph* graph);
 
 #endif
