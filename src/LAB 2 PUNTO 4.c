0efe0d5afd199fc20c411aaf7b888a5636cba112
paugaarridoo <pau.garrido01@estudiant.upf.edu>
2025-05-29 17:09:33 +0200

    LAB 2.4.c

--- src/LAB 2 PUNTO 4.c
+++ src/LAB 2 PUNTO 4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "LAB 2 PUNTO 1.c"
+#include "LAB 2.1.c"
 #define MAX_RECENT_QUERIES 3
 
-typedef struct{                          //Estructura per a la cua de queries
+typedef struct{                          //Estructura para la cola de queries
     Query *queries[MAX_RECENT_QUERIES];
     int start;
     int count;
 } QueryQueue;
 
-void queue_init(QueryQueue *q){  // Inicialitzem la cua de queries
+void queue_init(QueryQueue *q){  //Inicializamos la cola de queries
     q->start = 0;
     q->count = 0;
     for (int i = 0; i < MAX_RECENT_QUERIES; ++i){
@@ -18,9 +18,9 @@ void queue_init(QueryQueue *q){  // Inicialitzem la cua de queries
     }
 }
 
-void queue_push(QueryQueue *q, Query *query) {  //Afegim una query a la cua
+void queue_push(QueryQueue *q, Query *query) {  //Añadimos una query a la cola
     int idx = (q->start + q->count) % MAX_RECENT_QUERIES;
-    if (q->count == MAX_RECENT_QUERIES) {                  //Alliberem la memoria de la query inicial si la cua està plena
+    if (q->count == MAX_RECENT_QUERIES) {                  //Liberamos memoria de la query incicial (más vieja) si la cola estaá llena
         free(q->queries[q->start]->query);
         free(q->queries[q->start]);
         q->start = (q->start + 1) % MAX_RECENT_QUERIES;
@@ -30,7 +30,7 @@ void queue_push(QueryQueue *q, Query *query) {  //Afegim una query a la cua
     q->count++;
 }
 
-void queue_print(QueryQueue *q) {             //Imprimim les últimes queries de la cua
+void queue_print(QueryQueue *q) {             //Imprimimos las ultimas queries de la cola
     printf("Últimas %d queries:\n", q->count);
     for (int i = 0; i < q->count; ++i) {
         int idx = (q->start + i) % MAX_RECENT_QUERIES;
