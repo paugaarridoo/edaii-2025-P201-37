8b1185659ae7b853cf715f3716ad2eac3f70109a
TKone201 <leon.bescos01@estudiant.upf.edu>
2025-05-29 16:25:33 +0200

    Update LAB 4 PUNTO 2.c

--- src/LAB 4 PUNTO 2.c
+++ src/LAB 4 PUNTO 2.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include "LAB 1 PUNTO 3.c"
-#include "LAB4PUNTO1.c"
 
-int score_in_graph(Directedgraph* graph, Document *document){
-    int scores_array[graph->num_nodes];
-    for (int i = 0; i < graph->num_nodes; ++i){
-        scores_array[i] = 0;
-    }
-    for (int i = 0; i < graph->num_nodes; ++i){
-        for (int j = 0; j < graph->num_nodes; ++j){
-            if (graph->matrix[i][j] == 1){
-                scores_array[j]++;
-            }
-        }
-    }
-    return scores_array[document->id];
-}
